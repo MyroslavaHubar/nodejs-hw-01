2093153d8d0ff81d79532d55c8eb81553ba8706e
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-09-30 22:46:56 +0200

    update generateContacts js file

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+    for (let i = 1; i <= number; i += 1) {
+      contacts.push(createFakeContact());
+    }
+    await writeContacts(contacts);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 generateContacts(5);
