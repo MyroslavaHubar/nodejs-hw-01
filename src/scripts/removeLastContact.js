825c1b5036f37920cf1f1ccd21ce348e01cb5b52
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-10-01 00:12:28 +0200

    update remove files

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,7 +1,14 @@
 import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
 export const removeLastContact = async () => {
-  const contacts = await readContacts();
+  try {
+    const contacts = await readContacts();
+    contacts.pop();
+    await writeContacts(contacts);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 removeLastContact();
