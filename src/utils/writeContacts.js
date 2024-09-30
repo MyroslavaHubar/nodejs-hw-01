fab04fead82f63b557e19d6da5dd2c3fdcdb14df
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-09-30 22:35:38 +0200

    add utils and create contacts js file

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const newContacts = await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(updatedContacts, null, 2),
+    );
+    return newContacts;
+  } catch (error) {
+    console.error(error);
+  }
+};
